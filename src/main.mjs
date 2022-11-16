f339620f688b37dd396829594f3de6811a6212bc
Nico Müller <nicomueller1991@googlemail.com>
2022-11-16 23:16:07 +0100

    Fix motion sensor logic

--- src/main.mjs
+++ src/main.mjs
@@ -201,14 +201,17 @@ async function main() {
     
     // motion light
     const motionDetected$ = entityIds => anyBooleanEntityTrue$(entityIds)
-    const motionGone$ = entityIds => motionDetected$(entityIds)
-        // all entities have to be off
-        .map(R.not)
         // presence gone event will be sent only after 10 minutes of inactivity
         .debounce(600 * 1000)
 
+    const motionGone$ =  entityIds => motionDetected$(entityIds)
+        .map(R.not)
+
     const motionLight = (id, motionSensors, luminousitySensors, allLights, reactiveLights, nightReactiveLights) => {
-        const enableMotionLight$ = motionDetected$(motionSensors)
+        const motionInRoom = motionDetected$(motionSensors)
+        const motionGoneInRoom = motionGone$(motionSensors)
+
+        motionInRoom
             // do not react on motionDetected = false events
             .filter(R.equals(true))
             .onValue(streamLogger(`${id} motion detected`))
@@ -220,8 +223,8 @@ async function main() {
             .onValue(_ => turnLightsOn(reactiveLights))
             .onValue(_ => {
                 // turn lights off (later)
-                motionGone$(motionSensors)
-                    // do not react on motionGone = false events
+                motionGoneInRoom
+                    // only react on motion gone events
                     .filter(R.equals(true))
                     .onValue(streamLogger(`${id} turn lights off`))
                     .onValue(_ =>  turnLightsOff(allLights))

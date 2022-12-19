e687d7debeb821dd58c53d22ecbb7a52f3291dca
Nico Müller <nicomueller1991@googlemail.com>
2022-12-19 14:30:07 +0100

    Fix harmony one off activity renamed to "power_off"

--- src/main.mjs
+++ src/main.mjs
@@ -434,17 +434,20 @@ async function main() {
 
     // media automation scenes
     const mediaOn$ = entityState$("select.harmony_hub_activities")
-        .map(R.equals("PowerOff"))
+        .onValue(streamLogger(`media activity`))    
+        .map(R.equals("power_off"))
         .map(R.not)
         
     // toggle smart plug with harmony one activities
     mediaOn$  
         .filter(R.equals(true))
+        .onValue(streamLogger(`media activity started, turn on plug`))
         .onValue(_ => switchTurnOn("switch.plug_osram_2"))
         
     mediaOn$
         .debounce(minutes(2))
         .filter(R.equals(false))
+        .onValue(streamLogger(`media activity stopped, turn off plug`))
         .onValue(_ => switchTurnOff("switch.plug_osram_2"))
 
     const mediaAutomationId = "forbid_lights_when_tv_on";

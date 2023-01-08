bd11fe2160fdcdbcc594d88e4ff7712ae81aa9c4
Nico Müller <nicomueller1991@googlemail.com>
2023-01-08 22:08:21 +0100

    Add vacuum auto clean automation

--- src/main.mjs
+++ src/main.mjs
@@ -556,6 +556,24 @@ async function main() {
 
     const christmasRemote = tradfriRemoteSmall("sensor.remote_tradfri_small_3_action")
     switchLightsWithEvents(christmasRemote.on$, christmasRemote.off$, christmasLights)
+
+    // vacuum
+    const isOlderThanToday = date => {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        return date < today;
+    }
+
+    const cleanRequired$ = entityState$("sensor.roborock_vacuum_s6_last_clean_end")
+        .map(Date.parse)
+        .map(isOlderThanToday)
+
+    const vacuumAutoCleanId = "vacuum_auto_clean"
+    const startVacuum = () =>  haRx.callService("vacuum", "start", { entity_id: "vacuum.roborock_vacuum_s6" })
+    awayState$
+        .thru(filterAutomationEnabled(vacuumAutoCleanId))
+        .thru(filterByLogged(`${vacuumAutoCleanId} clean required`, cleanRequired$))
+        .onValue(_ => startVacuum())
 }
 
 main()

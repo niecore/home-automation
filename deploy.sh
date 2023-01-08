2a71f220015d5931c4842546cc36aae8294d3aec
Nico Müller <nicomueller1991@googlemail.com>
2023-01-08 21:32:35 +0100

    Add docker compose pull to deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@
 docker_host=$1
 
 docker buildx build --file ./Dockerfile --tag niecore/home-automation:latest --push .
+DOCKER_HOST=$docker_host docker-compose pull
 DOCKER_HOST=$docker_host docker-compose up -d --build

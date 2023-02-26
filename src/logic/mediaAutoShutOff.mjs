8e1e3d41a81af0caabb9dd82f8942e25be2abd0f
Nico Müller <nicomueller1991@googlemail.com>
2023-02-27 00:10:53 +0100

    MedaAutoShutOff also when no movment happend before

--- src/logic/mediaAutoShutOff.mjs
+++ src/logic/mediaAutoShutOff.mjs
@@ -1,3 +1,4 @@
+import * as R from "ramda";
 import { entityId } from "../homeassistant/entities.mjs";
 import { selectOption } from "../homeassistant/services.mjs";
 import { home } from "../domains/home.mjs";
@@ -7,6 +8,7 @@ import { filterAutomationEnabled } from "../domains/automations.mjs";
 import { debounceValue } from "../utils/stream.mjs";
 import { minutes } from "../utils/duration.mjs";
 import { streamLogger } from "../utils/logger.mjs";
+import { mediaOn$ } from "./mediaStandby.mjs";
 
 const motionsensorsInLivingRoom = home
     .filter(isMotionSensor)
@@ -15,8 +17,14 @@ const motionsensorsInLivingRoom = home
 
 const mediaAutoOffAutomationId = "media_auto_off";
 
-motionGone$(motionsensorsInLivingRoom)
+const noMotionInLivingRoom$ = motionGone$(motionsensorsInLivingRoom)
     .thru(debounceValue(true, minutes(30)))
+
+const mediaOnToLong$ = mediaOn$ 
+    .thru(debounceValue(true, minutes(30)))
+
+mediaOnToLong$.combine(noMotionInLivingRoom$, R.and)
+    .filter(R.equals(true))    
     .thru(filterAutomationEnabled(mediaAutoOffAutomationId))
     .onValue(streamLogger(`${mediaAutoOffAutomationId}: media on for 30 minutes without movement, turning device off`))
     .onValue(_ => selectOption("select.harmony_hub_activities", "power_off"))
